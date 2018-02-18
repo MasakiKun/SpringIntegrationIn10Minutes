701efee04d64a1ae6af58b624cd3c96c42d5da5d
마사키군 <ayukawa@MasakiKun-UI-MacBook-Pro.local>
2018-02-18 17:44:48 +0900

    Step 5: Send and receive a Spring Integration Message

--- src/main/java/kr/ayukawa/Bootstrap.java
+++ src/main/java/kr/ayukawa/Bootstrap.java
@@ -1,9 +1,17 @@
 package kr.ayukawa;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.PollableChannel;
+import org.springframework.messaging.support.MessageBuilder;
 
 public class Bootstrap {
 	public static void main(String[] args) {
-		new ClassPathXmlApplicationContext("applicationContext.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		PollableChannel channel = (PollableChannel)context.getBean("channel");
+		channel.send(MessageBuilder.withPayload("Spring Integration rocks").build());
+		Message<?> reply = channel.receive();
+		System.out.println("received: " + reply);
 	}
 }
