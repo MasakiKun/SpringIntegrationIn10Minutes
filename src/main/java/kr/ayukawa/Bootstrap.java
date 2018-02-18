4cec574d41bcb9182a0aef855d5948978fcec152
마사키군 <ayukawa@MasakiKun-UI-MacBook-Pro.local>
2018-02-18 17:53:49 +0900

    Step 8: Invoke the Service by sending a Message

--- src/main/java/kr/ayukawa/Bootstrap.java
+++ src/main/java/kr/ayukawa/Bootstrap.java
@@ -3,15 +3,17 @@ package kr.ayukawa;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.PollableChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
 public class Bootstrap {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		PollableChannel channel = (PollableChannel)context.getBean("channel");
-		channel.send(MessageBuilder.withPayload("Spring Integration rocks").build());
-		Message<?> reply = channel.receive();
+		MessageChannel input = (MessageChannel)context.getBean("input");
+		PollableChannel output = (PollableChannel)context.getBean("output");
+		input.send(MessageBuilder.withPayload("Spring Integration rocks").build());
+		Message<?> reply = output.receive();
 		System.out.println("received: " + reply);
 	}
 }
